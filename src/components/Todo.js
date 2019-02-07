9be7052d4ed6b7afbe679e0cf7ec025eda467866
Gareth Cassin <nissacjg@gmail.com>
2019-02-07 23:10:53 +0000

    change card size and and bottom border

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Todo extends Component {
   render() {
     return (
-      <div className='card-body'>
+      <div className='card-body' style={{ padding: '0.7rem' }}>
         <li className='text-center d-flex justify-content-around align-items-center' >
           <span className='col-md-9'>{this.props.task}</span>
           <button name='delete' className='btn btn-outline-secondary' type='button' onClick={() => this.props.delete(this.props.id)}>X</button>
